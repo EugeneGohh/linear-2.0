6c1bc60f657e21393bb8c58e71e128f7ae8cdf9a
Eugene Goh <64187129+EugeneGohh@users.noreply.github.com>
2023-02-26 13:47:53 +0800

    feat: add new components

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,6 +1,6 @@
 // import { delay } from "@/lib/async";
-// import Greeting from "@/components/Greeting";
-// import GreetingsSkeleton from "@/components/GreetingsSkeleton";
+import Greeting from "@/components/Greeting";
+import GreetingsSkeleton from "@/components/GreetingsSkeleton";
 // import NewProject from "@/components/NewProject";
 // import ProjectCard from "@/components/ProjectCard";
 // import TaskCard from "@/components/TaskCard";
@@ -35,9 +35,9 @@ export default async function Home() {
     <div className="h-full overflow-y-auto pr-6 w-full">
       <div className=" h-full items-stretch justify-center min-h-[content]">
         <div className="flex-1 grow flex">
-          {/* <Suspense fallback={<GreetingsSkeleton />}>
+          <Suspense fallback={<GreetingsSkeleton />}>
             <Greeting />
-          </Suspense> */}
+          </Suspense>
         </div>
         <div className="flex flex-2 grow items-center flex-wrap mt-3 -m-3 ">
           {/* {projects.map((project) => (
